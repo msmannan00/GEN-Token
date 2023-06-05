6ae4b91f444ddc681310b823ae41d0f7f6f7a06a
Abdul Mannan Saeed <msmannan00@protonmail.com>
2023-06-06 00:58:13 +0500

    minor db bug fixes

--- backend/app.py
+++ backend/app.py
@@ -3,25 +3,29 @@ from flask import Flask, jsonify, request
 import yaml
 from router import register_routes
 from flask_cors import CORS
+import json
 
 class Server(Flask):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.config['MONGO_URI'] = 'mongodb://localhost:27017/assesment'  # Update with your MongoDB URI
+        self.config['MONGO_URI'] = 'mongodb://localhost:27017/assessment'  # Update with your MongoDB URI
         self.mongo = PyMongo(self)
+
+        # Create collection from JSON file if it doesn't exist
+        with self.app_context():
+            db = self.mongo.db
+            collection = db['transactions']
+
+            with open('db/mongodb.json', 'r') as f:
+                # Load the JSON data as a list of dictionaries
+                data = json.load(f)
+
+                # Insert the default data into the collection
+                collection.insert_many(data)
 
 def create_app():
     app = Server(__name__)
     CORS(app)  # Enable CORS
-
-    # Load configuration from YAML file
-    def load_config_from_yaml(app, filename):
-        with open(filename, 'r') as f:
-            config = yaml.safe_load(f)
-            app.config['MONGO_URI'] = config['mongo_uri']
-
-    # Load configurations
-    load_config_from_yaml(app, 'database.yaml')
 
     # Register the routes
     register_routes(app)
